0f2ea71b8abac7747b64d3d6a11b6052e5250614
vijayan97 <vijaya@devbeeit.com>
2024-03-05 16:23:28 +0530

    minor changes

--- lib/custom_widgets/dashboard_app_bar.dart
+++ lib/custom_widgets/dashboard_app_bar.dart
@@ -4,6 +4,7 @@ import 'package:demo_school_wysax/ui/menus_search_screen.dart';
 import 'package:demo_school_wysax/ui/profile_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import '../constants/constants.dart';
 
@@ -18,10 +19,13 @@ class DashboardAppBar extends StatefulWidget {
 class _DashboardAppBarState extends State<DashboardAppBar> {
 
   GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
+  GlobalKey<ScaffoldState> _scaffoldkey = GlobalKey();
   int _page = 0;
-  @override
+
+   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        key: _scaffoldkey,
         appBar: AppBar(
           title: Text(Constants.appName, style: TextStyle(
               color: Theme.of(context).primaryColor,
@@ -76,7 +80,7 @@ class _DashboardAppBarState extends State<DashboardAppBar> {
         body: Stack(
           children: [
             _page == 0 ? HomeScreen() : _page == 1 ? MenusSearchScreen() :
-                _page == 3 ? ProfileScreen() : Container(
+                _page == 3 ? ProfileScreen() :  Container(
                   child: Text("$_page"),
                 )
           ],
@@ -96,35 +100,89 @@ class _DashboardAppBarState extends State<DashboardAppBar> {
 
           ],
           onTap: (index) {
+            if(index == 2)
+            {
+              _scaffoldkey.currentState?.showBottomSheet((_) => Container(
 
-            setState(() {
-              if(index == 2)
-                {
-                  showModalBottomSheet<void>(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return SizedBox(
-                        height: 200,
-                        child: Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: const <Widget>[
-                              Text('Layer'),
-                            ],
-                          ),
-                        ),
-                      );
-                    },
-                  );
-                }
+                color: Colors.white,
+                height: MediaQuery.of(context).size.height / 4,
+                width: MediaQuery.of(context).size.width,
+                child: Text('this is a bottomsheet'),
+              ));
+              // showQuickAccessMenus();
+            }
               else
-                {
+              {
+                setState(() {
                   _page = index;
-                }
-            });
-
+                });
+              }
           },
         ),
+    );
+  }
+
+   showQuickAccessMenus() {
+    showModalBottomSheet<void>(
+      useRootNavigator: true,
+      context: context,
+      builder: (BuildContext context) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10),
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: 200,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+               crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                InkWell(
+                  onTap: ()
+                  {
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                    height: 10,
+                    width: 80,
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  showBrandsBottomSheet() {
+    return Container(
+      color: Colors.white,
+      height: MediaQuery.of(context).size.height / 2,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 5, top: 3),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: const Align(
+                    alignment: Alignment.topRight, child: Icon(Icons.close)),
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              height: 350,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
